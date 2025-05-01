af185ee8e012f777ef81064c6774f08c633c7349
tiagomartins03 <tiagomartins766@gmail.com>
2025-05-01 14:17:35 -0300

    Final project

--- EditorHtml/View.cs
+++ EditorHtml/View.cs
@@ -2,12 +2,12 @@ using System;
 using System.Text.RegularExpressions;
 using EditorHTML;
 
-namespace EdHtml 
+namespace EdHtml
 {
 
     public class View
     {
-         public static void Show(string text)
+        public static void Show(string text)
         {
             Console.Clear();
             Console.BackgroundColor = ConsoleColor.DarkBlue;
@@ -19,14 +19,36 @@ namespace EdHtml
             Console.WriteLine("---------");
             Console.ReadKey();
             Menu.Show();
- 
+
 
         }
 
         public static void Replace(string text)
         {
-                var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*/\s*strong");
-                Console.WriteLine(strong);
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*/\s*strong");
+            var words = text.Split(' ');
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            (
+                                (words[i].LastIndexOf('<') - 1) -
+                                 words[i].IndexOf('>')
+
+                            )
+                        )
+
+                    );
+                    Console.Write(" ");
+
+                }
+                else { Console.ForegroundColor = ConsoleColor.Black; Console.WriteLine(words[i]);}
+            }
         }
 
     }
